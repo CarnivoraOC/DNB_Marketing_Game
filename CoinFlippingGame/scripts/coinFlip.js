62d35902a6607f6c3603a435836d0091bc6c78c2
CarnivoreOC <odahumlung@gmail.com>
2017-06-05 15:21:36 +0200

    Added Progress bar function, placed spin element inside progress bar

--- CoinFlippingGame/scripts/coinFlip.js
+++ CoinFlippingGame/scripts/coinFlip.js
@@ -1,19 +1,33 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+    var spinArray = ['animation900', 'animation1080', 'animation1260', 'animation1440', 'animation1620', 'animation1800', 'animation1980', 'animation2160'];
 
-var spinArray = ['animation900','animation1080','animation1260','animation1440','animation1620','animation1800','animation1980','animation2160'];
-
-function getSpin() {
-    var spin = spinArray[Math.floor(Math.random()*spinArray.length)];
-return spin;
-}
-
-$('#coin').on('click', function(){
-    $('#coin').removeClass();
+    function getSpin() {
+        var spin = spinArray[Math.floor(Math.random() * spinArray.length)];
+        return spin;
+    }
+    $('#coin').on('click', function () {
+        $('#coin').removeClass();
+        setTimeout(function () {
+            $('#coin').addClass(getSpin());
+        }, 50);
+        
+        updatePoints();
+    });
     
-setTimeout(function(){
-$('#coin').addClass(getSpin());
-}, 100);
-
+    function updatePoints() {
+        $('#points').html(function (i, val) {
+            return val * 1 + 10
+        });
+    };
 });
 
-});
+
+/*$('#coin').click(function(){
+    
+    var point = 0;
+        point = point +10;
+        document.getElementById("Points").innerHTML=point; 
+    
+});*/
+
+
